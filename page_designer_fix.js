f6446a659cfef7ad68a7b298154a80073099503d
StefanRoess <Stefan.Roess@outlook.com>
2018-02-20 16:19:27 +0100

    Add files via upload
    changes the middle region with the right region in apex page designer

---
+++ page_designer_fix.js
@@ -0,0 +1,60 @@
+// Oracle APEX 5.0 Page Designer Pane-Swapper
+// www.oratronik.de 
+// 2015-07-10 by Filip van Vooren - v0.1
+//
+// Not for production use! For educational purposes only.
+//
+function pd_fix(){
+    'use strict'
+
+    //
+    // Exit if not in APEX Page Designer
+    //
+    if (typeof(window.pageDesigner) != 'object') {
+       return 0;
+    }   
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // We have the following setup in the DOM tree:
+    //
+    //  <DIV id="sp_right_content" class="a-Splitter">
+    //      <DIV id="top_col" class="a-PageColumn">...</DIV> <DIV class="a-Splitter-barH">...</DIV> <DIV id="right_col" class="a-PageColumn">...</DIV>
+    //  </DIV>
+    //
+    // Remarks:
+    // * Positioning of child DIV's of <DIV id="sp_right_content"> is absolute.
+    // * In the splitter widget, the private property before$ points to the DIV before <DIV class="a-Splitter-barH"></DIV>
+    // * In the splitter widget, the private property after$ points to the DIV following <DIV class="a-Splitter-barH"></DIV>
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    var C_MIDDLE_PANE      = 'div#a_PageDesigner div#top_col',                                 // Page Builder: middle pane (Grid Layout/Messages/Page Search/...)
+        C_PROP_PANE        = 'div#a_PageDesigner div#right_col',                               // Page Builder: right pane  (Properties Editor)
+        C_SPLIT_HANDLE     = "div#a_PageDesigner div.a-Splitter-barH[title='Right Splitter']", // Page Builder: splitter handle/separator
+        C_SP_RIGHT_CONTENT = 'div#sp_right_content';                                           // Page Builder: right pane parent DIV element
+        
+    //
+    // Step 1: Swap the middle pane and properties pane in the DOM tree. 
+    //
+    $(C_PROP_PANE).insertBefore(C_SPLIT_HANDLE);
+    $(C_MIDDLE_PANE).insertAfter(C_SPLIT_HANDLE);
+                    
+    //
+    // Step 2: Recreate the splitter
+    //
+    // Recreating the splitter is required due to multiple reasons:
+    // 1. We can't change the "positionedFrom" property after the splitter is initialized.
+    // 2. We can't manipulate the "before$" and "after$" private properties in the splitter widget.
+    //
+    // See APEX splitter widget for details: /images/libraries/apex/widget.splitter.js
+    //  
+    var l_width_visual    = $(C_MIDDLE_PANE).width();
+    var l_width_props     = $(C_PROP_PANE).width(); 
+    var l_width_separator = $(C_SPLIT_HANDLE).width();                                         // Get width of splitter separator (normally 8px)
+    var l_split_options   = $(C_SP_RIGHT_CONTENT).splitter('option');                          // Get splitter widget options
+
+    l_split_options.positionedFrom = "begin";                                                  // We change this from "end" to "begin"
+    l_split_options.position       = l_width_props;                                            // Re-position splitter separator
+    $(C_SP_RIGHT_CONTENT).splitter('destroy');                                                 // Remove existing splitter JS object & DOM object
+    apex.jQuery(C_SP_RIGHT_CONTENT).splitter(l_split_options);                                 // Create new splitter JS object & DOM object using our stored options
+
+    return 1;
+}
